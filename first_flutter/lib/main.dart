8eec2ee12f83a3883b033b76a2643d10a8763816
WryBank <work.warayut@gmail.com>
2022-04-25 01:12:17 +0700

    test

--- first_flutter/lib/main.dart
+++ first_flutter/lib/main.dart
@@ -1,11 +1,28 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(home : Text('Hello'),);
-
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Deal'),
+          ),
+          body: Column(
+            children: [
+              Text('Question : '),
+              RaisedButton(
+                child: Text("Answer 1 "),
+                onPressed: () => print('test'),
+              )
+            ],
+          )),
+    );
   }
 }
