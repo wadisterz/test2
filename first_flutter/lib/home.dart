c129f7916ad168ea81901817133ee72a69907cc1
WryBank <work.warayut@gmail.com>
2022-04-27 04:03:34 +0700

    desing

--- first_flutter/lib/home.dart
+++ first_flutter/lib/home.dart
@@ -4,18 +4,43 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-       Container(
-        decoration: BoxDecoration(color: Color.fromRGBO(133, 244, 255, 1)),
-        height: 120,
-      ),//Appbar
-       Container(
-        decoration: BoxDecoration(color: Color.fromRGBO(133, 244, 255, 1)),
-        height: 120,
-      ),//Appbar
-        //backgroundColor: Color.fromRGBO(133, 244, 255, 1),
-        ],)
-        );
+        appBar: AppBar(
+          title: Text("This is Appbar"),
+        ),
+        backgroundColor: Color.fromRGBO(133, 244, 255, 1),
+        body: Padding(
+            padding: const EdgeInsets.all(50),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    child: Column(
+                  children: [
+                    SizedBox(
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          child: SizedBox(
+                            height: 100,
+                            width: 100,
+                          )),
+                    ),
+                    SizedBox(
+                        child: Row(
+                      children: [
+                        SizedBox(
+                          child: ElevatedButton(onPressed: (){},
+                          child: Text("Register")),
+                        ),
+                        SizedBox(
+                          child: ElevatedButton(onPressed: (){},
+                          child: Text("Back")),
+                        )
+                      ],
+                    ))
+                  ],
+                ))
+              ],
+            )));
   }
 }
