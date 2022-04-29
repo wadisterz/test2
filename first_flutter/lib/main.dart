19118d7fd7be0eff46364957ce04fb00519553d1
WryBank <work.warayut@gmail.com>
2022-04-29 21:38:41 +0700

    addmain

---
+++ first_flutter/lib/main.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:first_flutter/Login.dart';
+import 'package:first_flutter/Register.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Deal_Demo',
+      home: LoginPage(),
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(133, 244, 255, 1),
+        backgroundColor: Color.fromRGBO(133, 244, 255, 1)
+      ),
+    );
+  }
+}
