58d986d639f7d10a976c3d1063e46daab4062b28
WryBank <work.warayut@gmail.com>
2022-05-23 01:23:46 +0700

    forpull

--- first_flutter/lib/Feed.dart
+++ first_flutter/lib/Feed.dart
@@ -35,7 +35,7 @@ class _FeedState extends State<FeedPage> {
 
   final formKey = GlobalKey<FormState>();
 
-  Profile profile = Profile(uid: '' ,username: '', password: '', email: '');
+  Profile profile = Profile(uid: '', username: '', password: '', email: '');
   bool _showPassword = true;
   String? _Cpassword;
 
@@ -54,29 +54,54 @@ class _FeedState extends State<FeedPage> {
           }
           if (snapshot.connectionState == ConnectionState.done) {
             return Scaffold(
+              backgroundColor: Color.fromRGBO(133, 244, 255, 1),
               appBar: AppBar(
-                //title: Text("test"),
-                backgroundColor:Color.fromRGBO(133, 244, 255, 1),
-                elevation: 0,
-                actions: [
-                  IconButton(onPressed: (){
-                  }, 
-                  icon: Icon((FontAwesomeIcons.bell)
-                  ),
-                  )
-                ],
-                leading:Padding(
-                  padding: const EdgeInsets.all(0),
-                  child: IconButton(onPressed: (){
+                  //title: Text("test"),
+                  backgroundColor: Color.fromRGBO(133, 244, 255, 1),
+                  elevation: 0,
+                  actions: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon((FontAwesomeIcons.bell)),
+                    )
+                  ],
+                  leading: Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: IconButton(
+                        onPressed: () {}, icon: Icon((FontAwesomeIcons.bars))),
+                  )),
+              body: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(66, 194, 255, 1),
+                          borderRadius: BorderRadius.circular(50),
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 7.0,
+                                color: Colors.black.withOpacity(0.4),
+                                offset: Offset(0, 3))
+                          ],
+                        ),
+                        height: 80,
+                        child: Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.red
+                                
+                              ),
+                              height: 70,
+                              width: 60,
+                            )
+                          ],
 
-                  },
-                 icon: Icon((FontAwesomeIcons.bars))
-                  ),
-                ) 
-                
-                
-              ),
-
+                        )
+                      ),
+                    ],
+                  )),
             );
           }
           return Scaffold(
